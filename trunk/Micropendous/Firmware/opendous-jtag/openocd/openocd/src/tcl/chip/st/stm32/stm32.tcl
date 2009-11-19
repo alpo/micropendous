da02444425f8e74e8f01f184ae59c0b4af618489
opendous <opendous@a41975a6-add9-11de-9db0-312b4228ed8f>
2009-11-19 04:25:09 +0000

    don't need full SVN of OpenOCD as archived version exists

--- trunk/Micropendous/Firmware/opendous-jtag/openocd/openocd/src/tcl/chip/st/stm32/stm32.tcl
+++
@@ -1,7 +0,0 @@
-source [find tcl/bitsbytes.tcl]
-source [find tcl/cpu/arm/cortex_m3.tcl]
-source [find tcl/memory.tcl]
-source [find tcl/mmr_helpers.tcl]
-
-source [find tcl/chip/st/stm32/stm32_regs.tcl]
-source [find tcl/chip/st/stm32/stm32_rcc.tcl]
