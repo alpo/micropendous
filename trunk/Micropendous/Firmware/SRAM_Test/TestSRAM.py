18636dc40943df02d45c7eda95239607308ef132
opendous <opendous@a41975a6-add9-11de-9db0-312b4228ed8f>
2010-06-05 18:24:11 +0000

    Updated Micropendous-1287 project and TestSRAM.py external SRAM testing software.

--- trunk/Micropendous/Firmware/SRAM_Test/TestSRAM.py
+++ trunk/Micropendous/Firmware/SRAM_Test/TestSRAM.py
@@ -1,14 +1,16 @@
 #    Purpose: Test External SRAM on a Micropendous board with random data
 #        Visit www.Micropendous.org for more info.
 #    Created: 2009-08-23 by Opendous Inc.
-#    Last Edit: 2009-09-02 by Opendous Inc.
+#    Last Edit: 2010-06-04 by Opendous Inc.
 #    Released under the MIT License
 #
 # Expected Output on correct function of a Micropendous with an AT90USB647
 #   Serial<id=0xbad2b0, open=True>(port='COM10', baudrate=9600, bytesize=8, ...
-#   There are 61183 bytes of available SRAM in each of two banks for a total of 122366 bytes
+#   There are 57087 bytes of available SRAM in each of two banks for a total of 114174 bytes
 #   Micropendous Internal SRAM Test Status: SRAM_Passed_Tests
-#   It took  612.187999964 seconds to test 122366 bytes of SRAM
+#   It took  342.858999968 seconds to test 114174 bytes of SRAM
+
+
 import serial               # for accessing the serial port on multiple platforms
 import baseconvert    # for printing in binary format
 import random, re, time, sys
@@ -23,13 +25,9 @@ def sendByteStream(device, data):
 # comport    is a string - the Serial Port name to use
 # testMaxBytes    is an integer - the maximum number of bytes to test or 0 for all
 def TestSRAM(comport, testMaxBytes):
-    ser = serial.Serial(comport)         # open the 11th serial port, COM11 (counting starts at 0) on Windows
-    #ser = serial.Serial('/dev/ttyACM0') # under Linux, find serial port name with dmesg
-                                         # look for something like  cdc_acm 2-1:1.0: ttyACM0: USB ACM device
+    ser = serial.Serial(comport)         # open the given serial port
 
     print ser             # dump all info regarding serial port being used
-
-    # note  a = 0x61, f = 0x66, g = 0x67, i = 0x69,  s = 0x73
 
     # get the size of available external SRAM
     ser.write('s')
@@ -51,29 +49,43 @@ def TestSRAM(comport, testMaxBytes):
         Bank1Array.append(random.randint(0, 255))
         i = i + 1
 
+    # if user wants to only partially test external SRAM, reduce size of test
     if (int(testMaxBytes) > 0):
         sizeOfSRAM = int(testMaxBytes)
-    # fill each SRAM bank with corresponding data
+
+    # fill SRAM bank 0 with random data created above
     i = 0
+    ser.write('0') # select bank 0
     while (i < sizeOfSRAM):
+        # get current address bytes
         iByteMSB = ((i & 0xFF00) >> 8)
         iByteLSB = (i & 0x00FF)
-        # test Bank 0
-        ser.write('0')
+        # fill Bank 0 with data
         sendByteStream(ser, (0x67, iByteMSB, iByteLSB, Bank0Array[i]))
         #print "Bank0: ", ser.readline()
-        # test Bank 1
-        ser.write('1')
+        i = i + 1
+
+    # fill SRAM bank 1 with random data created earlier
+    i = 0
+    ser.write('1') # select bank 1
+    while (i < sizeOfSRAM):
+        # get current address bytes
+        iByteMSB = ((i & 0xFF00) >> 8)
+        iByteLSB = (i & 0x00FF)
+        # fill Bank 1 with data
         sendByteStream(ser, (0x67, iByteMSB, iByteLSB, Bank1Array[i]))
         #print "Bank1: ", ser.readline()
         i = i + 1
 
+
+    # test bank 0 to make sure it contains the appropriate data
     i = 0
+    ser.write('0')  # select bank 0
     while (i < sizeOfSRAM):
+        # get current address bytes
         iByteMSB = ((i & 0xFF00) >> 8)
         iByteLSB = (i & 0x00FF)
         # test Bank 0
-        ser.write('0')
         sendByteStream(ser, (0x69, iByteMSB, iByteLSB))
         returnedData = ser.readline()
         p = re.compile('\d+')
@@ -82,10 +94,17 @@ def TestSRAM(comport, testMaxBytes):
             print "SRAM Bank0 does not match sent data:", returnedData.strip(), \
                 "[", baseconvert.show_base(DataIntVal, 2, 8), "] should be", Bank0Array[i], \
                 "[", baseconvert.show_base(Bank0Array[i], 2, 8), "]"
-        #print returnedData # print the actual data returned from the Micropendous
+        #print returnedData     # show the actual data returned from the Micropendous
+        i = i + 1
 
-        # test Bank 1
-        ser.write('1')
+    # test bank 1 to make sure it contains the appropriate data
+    i = 0
+    ser.write('1')  # select bank 0
+    while (i < sizeOfSRAM):
+        # get current address bytes
+        iByteMSB = ((i & 0xFF00) >> 8)
+        iByteLSB = (i & 0x00FF)
+        # test Bank 0
         sendByteStream(ser, (0x69, iByteMSB, iByteLSB))
         returnedData = ser.readline()
         p = re.compile('\d+')
@@ -94,7 +113,7 @@ def TestSRAM(comport, testMaxBytes):
             print "SRAM Bank1 does not match sent data:", returnedData.strip(), \
                 "[", baseconvert.show_base(DataIntVal, 2, 8), "] should be", Bank1Array[i], \
                 "[", baseconvert.show_base(Bank0Array[i], 2, 8), "]"
-        #print returnedData # print the actual data returned from the Micropendous
+        #print returnedData     # show the actual data returned from the Micropendous
         i = i + 1
 
     ScriptEndTime = time.time()
@@ -105,8 +124,8 @@ def TestSRAM(comport, testMaxBytes):
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
-        print "Micropendous SRAM Test v2009-10-02 By Opendous Inc."
-        print "  Test the external SRAM on a Micropendous board."
+        print "Micropendous SRAM Test v2010-06-04 By Opendous Inc."
+        print "  Test the external SRAM on a Micropendous board which supports it."
         print "    Usage:"
         print "      python", sys.argv[0], "<port> <>"
         print "        Where <port> = serial port; COM? on Windows, '/dev/ttyACM0 on Linux'"
